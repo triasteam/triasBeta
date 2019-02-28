d6f407a90d67c3ce81af588d364a2df2fec17fe7
jorelin <719668442@qq.com>
2019-02-28 18:45:31 +0800

    headLine page style

--- html/components/common/HeadLine.js
+++ html/components/common/HeadLine.js
@@ -20,17 +20,61 @@ import $ from "jquery";
 export default class HeadLine extends React.Component {
     constructor(props) {
         super(props);
-        this.state={
+        this.state = {
 
         }
     }
-    
-    render(){
-        
+
+    render() {
+
         return (
-            <div className="sub-header"> 
+            <div className="sub-header">
                 <div className="headBar">
-                    {this.props.headBarName}
+                    <ul className="clearfix">
+                        <li className="sub-name">
+                            <p>{this.props.headBarName}</p>
+                        </li>
+                        <li className="sub-list clearfix">
+                            <div className="left-icon">
+                                <img src={require("../../img/icon/header/icon_header_currentEvent@2x.png")} alt="" />
+                            </div>
+                            <div className="right-sub">
+                                <p className="sub-title">
+                                    Current Event
+                                </p>
+                                <p className="sub-sub">
+                                    Hacker Attack
+                                </p>
+                            </div>
+                        </li>
+                        <li className="sub-list clearfix">
+                            <div className="left-icon">
+                                <img src={require("../../img/icon/header/icon_header_timeRemain@2x.png")} alt="" />
+                            </div>
+                            <div className="right-sub">
+                                <p className="sub-title">
+                                    Time Remain
+                                </p>
+                                <p className="sub-sub">
+                                    00:18:46
+                                </p>
+                            </div>
+                        </li>
+                        <li className="sub-list clearfix">
+                            <div className="left-icon">
+                                <img src={require("../../img/icon/header/icon_header_nextEvent@2x.png")} alt="" />
+                            </div>
+                            <div className="right-sub">
+                                <p className="sub-title">
+                                    Next Event
+                                </p>
+                                <p className="sub-sub">
+                                    Power Outage (20:30)
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+
                 </div>
             </div>
         )
