8e5b3aab24da20d2bfa57aebc70e8571d37fea26
Evelynzzz <15104026362@163.com>
2019-03-06 18:29:08 +0800

    Remove unused import

--- html/components/ChainSatus.js
+++ html/components/ChainSatus.js
@@ -1,58 +1,13 @@
 import React from "react";
-import $ from "jquery";
-// import ES6Promise from 'es6-promise'
-// ES6Promise.polyfill() //let IE6 support Promise object
-import {
-  injectIntl,
-  intlShape,
-  FormattedMessage
-} from "react-intl"; /* react-intl imports */
 import GeneralStatics from "./GeneralStatics";
 import RightPart from "./RightPart";
 import LineMarkCollection from "./charts/LineMarkCollection";
 import NodesGraph from "./NodesGraph";
 import EventHappenSwiper from "./common/EventHappenSwiper"
 /* Component for node status page */
-class ChainStatus extends React.Component {
+export default class ChainStatus extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      // lang: this.props.intl.locale,    // current locale language
-    };
-  }
-
-  // /**
-  //  * Before a mounted component receives new props, reset some state.
-  //  * @param {Object} nextProps new props
-  //  */
-  // componentWillReceiveProps(nextProps){
-  //     // if locale language will be changed, reset lang state
-  //     if(this.state.lang != nextProps.intl.locale){
-  //         this.setState({
-  //             lang: nextProps.intl.locale
-  //         })
-  //     }
-  // }
-
-  /**
-   * After the component is mounted.
-   * - Start the countdown clock.
-   * - Get data for this page.
-   */
-  componentDidMount() {
-    
-  }
-
-
-  
-
- 
-
-  /**
-   * When the component will be unmounted.
-   * Clear the intervals
-   */
-  componentWillUnmount() {
   }
 
   render() {
@@ -71,9 +26,3 @@ class ChainStatus extends React.Component {
     );
   }
 }
-/* Inject intl to ChainStatus props */
-const propTypes = {
-  intl: intlShape.isRequired
-};
-ChainStatus.propTypes = propTypes;
-export default injectIntl(ChainStatus);
