0e4f8215fc59d8183ff6a82d0a3ef2e78dc1e454
jorelin <719668442@qq.com>
2019-03-12 15:51:54 +0800

    Additional notes

--- html/components/common/EventHappenSwiper.js
+++ html/components/common/EventHappenSwiper.js
@@ -1,137 +1,150 @@
-import React from "react"
+import React from "react";
 
-import Swiper from 'swiper/dist/js/swiper.js'
-import 'swiper/dist/css/swiper.min.css'
-import TimeInterval from './TimeInterval'
+import Swiper from "swiper/dist/js/swiper.js";
+import "swiper/dist/css/swiper.min.css";
+import TimeInterval from "./TimeInterval";
 import $ from "jquery";
 /**
-
+ * The Subcomponent of the ChainStatus --- TimeLine
+ * Show the occurrence of events at different times
  */
 export default class EventHappenSwiper extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            eventList: [],
-            currentEventIndex: 0,
+  constructor(props) {
+    super(props);
+    this.state = {
+      eventList: [], // The list of events at different times
+      currentEventIndex: 0 // The ID of the event that is happening
+    };
+  }
+  /**
+   * initialize swiper carousel
+   */
+  createSwiper() {
+    var mySwiper = new Swiper(".swiper-container", {
+      // direction: 'vertical', // 垂直切换选项
+      loop: false, // 循环模式选项
+      slidesPerView: 5, // display time block
+      spaceBetween: 0,
+      initialSlide: this.state.currentEventIndex - 2,
+      // 如果需要前进后退按钮
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+
+      // 如果需要滚动条
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true
+      }
+    });
+  }
+  /**
+   * Get the list of events at different times
+   *
+   */
+  getTimeEvent() {
+    var self = this;
+    $.ajax({
+      url: "/api/event_list/ ",
+      type: "get",
+      dataType: "json",
+      data: {},
+      success: function(data) {
+        if (data.status == "success") {
+          self.setState({
+            currentEventIndex: data.result.current_index,
+            eventList: data.result.event_list
+          });
+          // After successfully obtaining the list,Initialize the swiper carousel
+          self.createSwiper();
+        } else {
+          // error
+          self.setState({
+            currentEventIndex: -1,
+            eventList: []
+          });
         }
-    }
-    createSwiper() {
-        var mySwiper = new Swiper('.swiper-container', {
-            // direction: 'vertical', // 垂直切换选项
-            loop: false, // 循环模式选项
-            slidesPerView: 5, // display time block
-            spaceBetween: 0,
-            initialSlide: this.state.currentEventIndex - 2,
-            // 如果需要前进后退按钮
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
+      },
+      error: function() {
+        self.setState({
+          currentEventIndex: -1,
+          eventList: []
+        });
+      }
+    });
+  }
 
-            // 如果需要滚动条
-            scrollbar: {
-                el: '.swiper-scrollbar',
-                hide: true,
-            },
-        })
-    }
-    getTimeEvent() {
-        var self = this
-        $.ajax({
-            url: '/api/event_list/ ',
-            type: 'get',
-            dataType: 'json',
-            data: {
-            },
-            success: function (data) {
-                if (data.status == 'success') {
-                    self.setState({
-                        currentEventIndex: data.result.current_index,
-                        eventList: data.result.event_list
-                    })
-                    self.createSwiper()
-                } else {
-                    self.setState({
-                        currentEventIndex: -1,
-                        eventList: []
-                    })
-                }
-            },
-            error: function () {
-                self.setState({
-                    currentEventIndex: -1,
-                    eventList: []
-                })
-            }
-        })
-    }
+  componentDidMount() {
+    this.getTimeEvent();
+  }
 
-    componentDidMount() {
-        this.getTimeEvent();
-    }
-
-    render() {
-
-        return (
-            <div className="eventSwiper">
-                <div className="swiper-container">
-                    <div className="swiper-wrapper">
-
-                        {
-                            this.state.eventList.length > 0 && this.state.eventList.map((event, index) => {
-
-                                return (
-                                    <div className="swiper-slide " key={index}>
-                                        {
-                                            this.state.currentEventIndex == index &&
-                                            <div className="eventCurrent">
-                                                <p className="startTime">{event.start}</p>
-                                                <p className="eventName">{event.name}</p>
-                                                <TimeInterval interval={event.interval} TimeChange={'add'} />
-                                            </div>
-                                        }
-                                        {
-                                            this.state.currentEventIndex != index &&
-                                            <div className={this.state.currentEventIndex > index ? "eventHappend" : "eventComming"}>
-                                                <p className="startTime">{event.start}</p>
-                                                <p className="eventName">{event.name}</p>
-                                                {
-                                                    this.state.currentEventIndex > index &&
-                                                    <TimeInterval interval={event.interval} TimeChange={'add'} />
-                                                }
-                                                {
-                                                    this.state.currentEventIndex < index &&
-                                                    <TimeInterval interval={event.interval} TimeChange={'decrease'} />
-                                                }
-                                            </div>
-                                        }
-                                    </div>
-                                )
-                            })
+  render() {
+    return (
+      <div className="eventSwiper">
+        <div className="swiper-container">
+          <div className="swiper-wrapper">
+            {this.state.eventList.length > 0 &&
+              this.state.eventList.map((event, index) => {
+                return (
+                  <div className="swiper-slide " key={index}>
+                    {this.state.currentEventIndex == index && (
+                      <div className="eventCurrent">
+                        <p className="startTime">{event.start}</p>
+                        <p className="eventName">{event.name}</p>
+                        <TimeInterval
+                          interval={event.interval}
+                          TimeChange={"add"}
+                        />
+                      </div>
+                    )}
+                    {this.state.currentEventIndex != index && (
+                      <div
+                        className={
+                          this.state.currentEventIndex > index
+                            ? "eventHappend"
+                            : "eventComming"
                         }
+                      >
+                        <p className="startTime">{event.start}</p>
+                        <p className="eventName">{event.name}</p>
+                        {this.state.currentEventIndex > index && (
+                          <TimeInterval
+                            interval={event.interval}
+                            TimeChange={"add"}
+                          />
+                        )}
+                        {this.state.currentEventIndex < index && (
+                          <TimeInterval
+                            interval={event.interval}
+                            TimeChange={"decrease"}
+                          />
+                        )}
+                      </div>
+                    )}
+                  </div>
+                );
+              })}
 
-                        {
-                            (!this.state.eventList || this.state.eventList.length == 0) && <p style={{ paddingLeft: '40px', width: '100%' }}>当前没有匹配的数据。</p>
-                        }
+            {(!this.state.eventList || this.state.eventList.length == 0) && (
+              <p style={{ paddingLeft: "40px", width: "100%" }}>
+                当前没有匹配的数据。
+              </p>
+            )}
+          </div>
 
+          {/* <!-- 如果需要导航按钮 --> */}
+          <div className="bg-cover-left">
+            <div className="swiper-button-prev" />
+          </div>
+          <div className="bg-cover-right">
+            <div className="swiper-button-next" />
+          </div>
 
-                    </div>
-
-                    {/* <!-- 如果需要导航按钮 --> */}
-                    <div className="bg-cover-left">
-                        <div className="swiper-button-prev"></div>
-                    </div>
-                    <div className="bg-cover-right">
-                        <div className="swiper-button-next"></div>
-                    </div>
-
-
-                    {/* <!-- 如果需要滚动条 --> */}
-                    <div className="swiper-scrollbar"></div>
-                </div>
-
-
-            </div>
-        )
-    }
+          {/* <!-- 如果需要滚动条 --> */}
+          <div className="swiper-scrollbar" />
+        </div>
+      </div>
+    );
+  }
 }
--- html/components/common/TimeInterval.js
+++ html/components/common/TimeInterval.js
@@ -1,31 +1,52 @@
 import React from "react"
 /*
+** The Subcomponent of the time axis ----- count down
 
-    
+** Provide time countdown function
+
+** Parent param
+
+* - Timestamp: {this.props.interval}
+* - Time to increase or decrease : {this.state.TimeChange}
 */
 export default class TimeInterval extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            timeCount: ''
+            timeCount: '',// Time format after timestamp processing --- xx : xx :xx
         }
     }
-
+    /**
+     * Handle timestamps , Countdown reduction
+     * @param {timestamps} times 
+     */
     transformTime(times) {
         this.interval(times)
     }
+    /**
+     * Handle timestamps , Countdown increased
+     * @param {timestamps} times 
+     */
     transformTimeAdd(times) {
         this.intervalAdd(times)
     }
+    /**
+     * setInterval function of time decrease
+     * @param {timestamps} times 
+     */
     interval(times) {
         this.timerChange = setInterval(() => {
             times--;
             this.changeTime(times)
-            if(times<=0){
+            if (times <= 0) {
                 clearInterval(this.timerChange)
             }
         }, 1000)
     }
+    /** 
+     * Timestamp format conversion
+     * @param {timestamps} times
+     */
     changeTime(times) {
         var day = 0,
             hour = 0,
@@ -43,28 +64,34 @@ export default class TimeInterval extends React.Component {
         if (second <= 9) second = '0' + second;
 
         this.setState({
-            timeCount:hour + ":" + minute + ":" + second
+            timeCount: hour + ":" + minute + ":" + second
         })
     }
+    /**
+     * setInterval function of time increased
+     * @param {timestamps} times 
+     */
     intervalAdd(times) {
         this.timerChange = setInterval(() => {
             times++;
             this.changeTime(times)
-            
+
         }, 1000)
     }
     componentDidMount() {
-        if(this.props.TimeChange == 'add'){
+        // Time to increase or decrease , execute different functions
+        if (this.props.TimeChange == 'add') {
             this.transformTimeAdd(this.props.interval)
-        }else{
+        } else {
             this.transformTime(this.props.interval)
         }
     }
 
-    componentWillUnmount(){
+    componentWillUnmount() {
+        // clear timer
         this.timerChange = clearInterval(this.timerChange)
     }
-    
+
     render() {
 
         return (
@@ -74,7 +101,7 @@ export default class TimeInterval extends React.Component {
                     this.props.interval > 0 &&
                     <span >{this.state.timeCount}</span>
                 }
-                
+
             </p>
 
 
