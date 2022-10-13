95f47ddd831b51895e52348744772da021878628
60ke <i505125461@live.com>
2022-10-13 19:50:52 +0800

    Adapt to python3.8

--- app_views/dispatcher.py
+++ app_views/dispatcher.py
@@ -2,12 +2,28 @@
 # middleware for dispatch url
 """
 
-from django.shortcuts import render_to_response
-
+from django.shortcuts import render
+import threading
+request_cfg = threading.local()
 
 class QtsAuthentication(object):
-    def process_request(self, request):
-        if 'api' in request.path:
-            pass
-        else:
-            return render_to_response('index.html')
+    def __init__(self, get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+        if 'api' not in request.path:
+            return render(request,'index.html')
+
+        db_name =  request.GET.get('chain','default')
+        if db_name not in ('default','tm'):
+            db_name = 'default'
+        request_cfg.db = db_name
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
