fc97bd0c2cf1cef565b0ae592d3ac985f40dce25
lsk <i505125461@live.com>
2023-01-09 14:28:35 +0800

    fix QtsAuthentication err

--- app_views/dispatcher.py
+++ app_views/dispatcher.py
@@ -6,8 +6,10 @@ from django.shortcuts import render
 
 
 class QtsAuthentication(object):
+    def __init__(self, get_response):
+        self.get_response = get_response    
     def process_request(self, request):
         if 'api' in request.path:
             pass
         else:
-            return render('index.html')
+            return render(request,'index.html')
